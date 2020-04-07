e1fb5ae7fda78105434ae07634aa932ca4c1f7b3
mcheng8 <50517517+mcheng8@users.noreply.github.com>
2020-04-07 12:50:00 -0400

    Add files via upload

---
+++ D3.L7 - Classes&Objects - Projects/PackerExample/PackerExample/main.swift
@@ -0,0 +1,9 @@
+//  Step 5.  Instantiation by calling the class and declaring values for the input arguments.
+var cannedPrank = PackerC(itemType: "fake snake")
+
+//  Step 6. Accessing the properties of the object by calling the method (via the dot notation)
+print(cannedPrank.containerType)
+print(cannedPrank.stuffInside)
+
+//  Step 7. Accessing the action of the object by calling the method (via the dot notation)
+cannedPrank.description()
---
+++ D3.L7 - Classes&Objects - Projects/PracticeExample/PracticeExample/main.swift
@@ -0,0 +1,4 @@
+var myCar = CarFactory(chooseColor: "Red", chooseNumOfDoor: 2, chooseCarStyle: "Sedan", chooseOptions: "GPS")
+
+myCar.standardInfo()
+myCar.optionalInfo()
