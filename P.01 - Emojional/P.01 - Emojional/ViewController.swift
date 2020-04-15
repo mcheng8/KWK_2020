7ae91913cddb69347e42243dda8366deb62f842d
mcheng8 <50517517+mcheng8@users.noreply.github.com>
2020-04-15 05:58:51 -0400

    Add files via upload
    Completed Iteration 1

--- P.01 - Emojional/P.01 - Emojional/ViewController.swift
+++ P.01 - Emojional/P.01 - Emojional/ViewController.swift
@@ -15,6 +15,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showMessage(sender: UIButton) {
+        let messageControl = UIAlertController(title: "This is a test", message: "If you see this, it works!", preferredStyle: UIAlertController.Style.alert)
+        
+        messageControl.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(messageControl, animated: true, completion: nil)
+    }
+    
+    
+    
 }
 
