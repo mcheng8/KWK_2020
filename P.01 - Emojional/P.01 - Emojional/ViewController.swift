4c11af891211b3112456b28f8cf699516f95c9e2
mcheng8 <50517517+mcheng8@users.noreply.github.com>
2020-04-15 15:25:50 -0400

    Add files via upload
    Completed Iterations 3 & 4.

--- P.01 - Emojional/P.01 - Emojional/ViewController.swift
+++ P.01 - Emojional/P.01 - Emojional/ViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     let availableEmotions = ["😑":"bored","😔":"sad"]
-        
+    let customMessages = ["bored":["You are making a difference in the world by staying home!","You are saving lives by staying home!","Do the best that you can, one day at a time!"],"sad":["It is okay, you are not the only one who feels this way.","Everything that is going is upsetting for everyone.","I'm so glad that you tapped me!"]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -20,14 +21,18 @@ class ViewController: UIViewController {
     @IBAction func showMessage(sender: UIButton) {
         if let selectedEmotion = sender.titleLabel?.text{
         
-        let titleText = selectedEmotion
-        let messageText = availableEmotions["\(selectedEmotion)"]!
+            let titleText = selectedEmotion
+            let messageText = availableEmotions["\(selectedEmotion)"]!
+        
+            let randomNumber = Int.random(in: 0...2)
             
-        let messageControl = UIAlertController(title: "\(titleText)", message: "\(messageText)", preferredStyle: UIAlertController.Style.alert)
+            let selectedMessage = customMessages[availableEmotions[selectedEmotion]!]![randomNumber]
+            
+            let messageControl = UIAlertController(title: "\(titleText)", message: "\(selectedMessage)", preferredStyle: UIAlertController.Style.alert)
         
-        messageControl.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            messageControl.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         
-        present(messageControl, animated: true, completion: nil)
+            present(messageControl, animated: true, completion: nil)
         }
     }
     
