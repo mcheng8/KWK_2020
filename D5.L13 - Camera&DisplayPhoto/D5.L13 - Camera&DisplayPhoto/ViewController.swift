18bc048b3b92005b042a464d8b9ad70b498a399a
mcheng8 <50517517+mcheng8@users.noreply.github.com>
2020-04-20 16:09:50 -0400

    Add files via upload
    Completed Iteration 1 & 2

--- D5.L13 - Camera&DisplayPhoto/D5.L13 - Camera&DisplayPhoto/ViewController.swift
+++ D5.L13 - Camera&DisplayPhoto/D5.L13 - Camera&DisplayPhoto/ViewController.swift
@@ -17,6 +17,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imageChoosen.delegate = self
     }
 
+    @IBAction func takeSelfieTapped(_ sender: UIButton) {
+        imageChoosen.sourceType = .camera
+        present(imageChoosen, animated: true, completion: nil)
+    }
+    
+    @IBAction func photoLibraryTapped(_ sender: UIButton) {
+        imageChoosen.sourceType = .photoLibrary
+        present(imageChoosen, animated: true, completion: nil)
+    }
 
+    @IBOutlet weak var displayImage: UIImageView!
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {displayImage.image = selectedImage}
+            imageChoosen.dismiss(animated: true, completion: nil)
+    }
 }
+
 
