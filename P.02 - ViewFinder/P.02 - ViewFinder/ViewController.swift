ccaa749f1037bf3ccc091885a60be6fd28cb5d67
mcheng8 <50517517+mcheng8@users.noreply.github.com>
2020-04-21 10:52:59 -0400

    Add files via upload
    Completed Iteration 0

---
+++ P.02 - ViewFinder/P.02 - ViewFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  P.02 - ViewFinder
+//
+//  Created by Mark Cheng on 4/21/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
