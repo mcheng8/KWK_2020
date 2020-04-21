ac9180b8ef0942bd4ba28f09232bf58899db2830
mcheng8 <50517517+mcheng8@users.noreply.github.com>
2020-04-21 13:06:55 -0400

    Completed Iteration 1b & 1c

--- P.02 - ViewFinder/P.02 - ViewFinder/AddPhotoViewController.swift
+++ P.02 - ViewFinder/P.02 - ViewFinder/AddPhotoViewController.swift
@@ -16,4 +16,24 @@ class AddPhotoViewController: UIViewController, UIImagePickerControllerDelegate,
         super.viewDidLoad()
         imageChoosen.delegate = self
     }
+    
+    @IBAction func cameraTapped(_ sender: UIButton) {
+        imageChoosen.sourceType = .camera
+        present(imageChoosen, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func albumTapped(_ sender: UIButton) {
+        imageChoosen.sourceType = .photoLibrary
+        present(imageChoosen, animated: true, completion: nil)
+    }
+    
+    @IBOutlet weak var displayImage: UIImageView!
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {displayImage.image = selectedImage}
+        
+        imageChoosen.dismiss(animated: true, completion: nil)
+    }
 }
