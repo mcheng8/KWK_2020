f09a5fdec9a89c2487be116ae3b411db4c954ee9
mcheng8 <50517517+mcheng8@users.noreply.github.com>
2020-04-27 14:51:06 -0400

    Completed Iteration 0

---
+++ P.03.ToDoList/P.03.ToDoList/ToDoClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoClass.swift
+//  P.03.ToDoList
+//
+//  Created by Mark Cheng on 4/27/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import Foundation
+
+class ToDoClass {
+    
+    var description = ""
+    var important = false
+}
---
+++ P.03.ToDoList/P.03.ToDoList/ToDoTableViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ToDoTableViewController.swift
+//  P.03.ToDoList
+//
+//  Created by Mark Cheng on 4/27/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+    
+    func createToDo() -> [ToDoClass] {
+        
+        let swiftToDo = ToDoClass()
+        swiftToDo.description = "Learn Swift"
+        swiftToDo.important = true
+        
+        let dogToDo = ToDoClass()
+        dogToDo.description = "Walk the Dog"
+        
+        let kwkToDo = ToDoClass()
+        kwkToDo.description = "Complete Technical #6"
+        kwkToDo.important = true
+        
+        return[swiftToDo,dogToDo,kwkToDo]
+    }
+    
+    var listOfToDo : [ToDoClass] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        listOfToDo = createToDo()
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 3
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        let eachToDo = listOfToDo[indexPath.row]
+        
+        if eachToDo.important {
+            cell.textLabel?.text = "❗️" + eachToDo.description
+        } else {
+            cell.textLabel?.text = eachToDo.description
+        }
+        
+        
+        return cell
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
